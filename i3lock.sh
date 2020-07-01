710247270831a3bc44c2ad6b6c02014ceeedd55b
Josenaldo <josenaldo.asj@gmail.com>
2020-07-01 16:40:37 -0300

    Change lockscreen script
    Select random lockscreen image

--- i3lock.sh
+++ i3lock.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Lockscreen image
-img="$HOME/Pictures/lockscreen.png"
+img_dir="$HOME/Pictures/pika/"
+img_name=$(ls $img_dir | shuf -n 1)
+img_path="${img_dir}${img_name}"
 
 # Run i3lock
-i3lock -i $img
+i3lock -i $img_path
 
